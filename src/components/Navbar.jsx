49117141bf7b21931da14dc574cb2f4c691922d9
Irina <irina.bojkovska.ib@gmail.com>
2025-04-03 12:33:55 -0700

    Fixed Git conflict and added files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Link } from 'react-router-dom';
 import '../css/Navbar.css';
 import { useState } from 'react';
@@ -44,50 +43,3 @@ export default function Navbar() {
     </header>
   );
 }
-=======
-import { Link } from 'react-router-dom';
-import '../css/Navbar.css';
-import { useState } from 'react';
-
-export default function Navbar() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMenuOpen((prevState) => {
-      return !prevState;
-    });
-  };
-
-  return (
-    <header className="navbar-container">
-      <img className="logo-img" src="../../public/shared/logo.svg" alt="" />{' '}
-      <img
-        className="hamburger-img"
-        src="../../public/shared/icon-hamburger.svg"
-        alt=""
-        onClick={toggleMenu}
-      />
-      <nav className={`navbar-nav ${isMenuOpen ? 'active' : ''}`}>
-        <img
-          onClick={toggleMenu}
-          className="close-icon"
-          src="../../public/shared/icon-close.svg"
-          alt=""
-        />
-        <Link className="nav_link" to="/">
-          <span>00</span> HOME
-        </Link>
-        <Link className="nav_link" to="/destination">
-          <span>01</span> DESTINATION
-        </Link>
-        <Link className="nav_link" to="/crew">
-          <span>02</span> CREW
-        </Link>
-        <Link className="nav_link" to="/technology">
-          <span>03</span> TECHNOLOGY
-        </Link>
-      </nav>
-    </header>
-  );
-}
->>>>>>> 57ddcef (Initial commit)
